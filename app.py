9ba75951776d6f21575176a0acfd3153f5d07f77
Tomas Ribeiro <up202007710@up.pt>
2025-04-30 11:47:32 +0100

    pagination

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import os
-import random
-from kdtree import get_recommendations
-
+from kdtree import get_recommendations  
 
 app = Flask(__name__)
 
+IMAGES_PER_PAGE = 100
+
 @app.route('/')
-def clothing():
-    images = os.listdir('static/images')
-    return render_template('home.html', images=images)
+@app.route('/page/<int:page>')
+def clothing(page=1):
+    images = sorted(os.listdir('static/images'))
+    total_images = len(images)
+    total_pages = (total_images - 1) // IMAGES_PER_PAGE + 1
+    start = (page - 1) * IMAGES_PER_PAGE
+    end = start + IMAGES_PER_PAGE
+    images_paginated = images[start:end]
+    return render_template('home.html', images=images_paginated, page=page, total_pages=total_pages)
 
 @app.route('/recommendations/<selected_image>')
 def recommendations(selected_image):
