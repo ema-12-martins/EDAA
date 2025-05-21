2b2a43fd10c16602941691b2483511a95a8008b6
Tomas Ribeiro <up202007710@up.pt>
2025-05-21 11:32:20 +0100

    eval

--- app.py
+++ app.py
@@ -10,11 +10,12 @@ IMAGES_PER_PAGE = 5
 @app.route('/page/<int:page>')
 def clothing(page=1):
     images = sorted(os.listdir('static/images'))
-    total_images = len(images)
+    images_info = [{'filename': img, 'id': img.split('.')[0]} for img in images]
+    total_images = len(images_info)
     total_pages = (total_images - 1) // IMAGES_PER_PAGE + 1
     start = (page - 1) * IMAGES_PER_PAGE
     end = start + IMAGES_PER_PAGE
-    images_paginated = images[start:end]
+    images_paginated = images_info[start:end]
     return render_template('home.html', images=images_paginated, page=page, total_pages=total_pages)
 
 @app.route('/recommendations/<selected_image>')
